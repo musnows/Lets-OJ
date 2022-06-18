0a27ba300051664ddd0cca7995f3b6639d484aae
Ewait <S2522446388@163.com>
2022-06-18 22:23:53 +0800

    上传早先的OJ刷题文件

---
+++ LetsOJ/ewait/Leetcode/数据结构_二叉树.c
@@ -0,0 +1,257 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//144. 二叉树的前序遍历
+//https://leetcode-cn.com/problems/binary-tree-preorder-traversal/submissions/
+//// 二叉树节点个数
+//void BTreeSize(struct TreeNode* root, int* pcount)
+//{
+//	if (root == NULL)
+//		return;
+//
+//	(*pcount)++;
+//	BTreeSize(root->left, pcount);
+//	BTreeSize(root->right, pcount);
+//
+//}
+//// 前序遍历代码
+//void BTreePrevOrder(struct TreeNode* root, int* arr, int* i)
+//{
+//	if (root == NULL) {
+//		return;
+//	}
+//
+//	arr[(*i)++] = root->val;
+//	//因为在递归调用中需要多次调用不同的i，所以需要取地址
+//	BTreePrevOrder(root->left, arr, i);
+//	BTreePrevOrder(root->right, arr, i);
+//}
+//
+//int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+//	int count = 0;
+//	BTreeSize(root, &count);//计算树节点个数
+//	int* arr = (int*)malloc(sizeof(int) * count);//开辟数组
+//
+//	int i = 0;//下标
+//	BTreePrevOrder(root, arr, &i);
+//	*returnSize = count;
+//	return arr;
+//}
+
+//94二叉树中序遍历
+//https://leetcode-cn.com/problems/binary-tree-inorder-traversal/submissions/
+//145. 二叉树的后序遍历
+//https://leetcode-cn.com/problems/binary-tree-postorder-traversal/submissions/
+//只需要更改数组位置就可以了！
+
+
+
+
+//100. 相同的树
+//https://leetcode-cn.com/problems/same-tree/
+//
+//bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+//    if (p == NULL && q == NULL)//比较是否两个节点都为空，都为空是真
+//        return true;
+//
+//    if (p == NULL || q == NULL)//如果有一个为空，另外一个非空，即为假
+//        return false;
+//
+//    if (p->val != q->val)//都不是空，判断val的值是否相等
+//        return false;
+//
+//    //递归判断左子树和右子树是否相等
+//    return isSameTree(p->left, q->left)
+//        && isSameTree(p->right, q->right);
+//}
+
+
+//101. 对称二叉树
+//https://leetcode-cn.com/problems/symmetric-tree/
+// 
+//bool _isSameTree(struct TreeNode* p, struct TreeNode* q) {
+//    if (p == NULL && q == NULL)//比较是否两个节点都为空，都为空是真
+//        return true;
+//
+//    if (p == NULL || q == NULL)//如果有一个为空，另外一个非空，即为假
+//        return false;
+//
+//    if (p->val != q->val)//都不是空，判断val的值是否相等
+//        return false;
+//
+//    //递归判断左子树和右子树是否对称相等
+//    return _isSameTree(p->left, q->right)
+//        && _isSameTree(p->right, q->left);
+//}
+//
+//bool isSymmetric(struct TreeNode* root) {
+//    return _isSameTree(root->left, root->right);
+//}
+
+
+
+//572. 另一棵树的子树
+//https://leetcode-cn.com/problems/subtree-of-another-tree/submissions/
+//bool _isSameTree(struct TreeNode* p, struct TreeNode* q) {
+//    if (p == NULL && q == NULL)//比较是否两个节点都为空，都为空是真
+//        return true;
+//
+//    if (p == NULL || q == NULL)//如果有一个为空，另外一个非空，即为假
+//        return false;
+//
+//    if (p->val != q->val)//都不是空，判断val的值是否相等
+//        return false;
+//
+//    //递归判断左子树和右子树是否相等
+//    return _isSameTree(p->left, q->left)
+//        && _isSameTree(p->right, q->right);
+//}
+//
+//bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot) {
+//    // if(root==NULL&&subRoot==NULL)
+//    //     return true;
+//    //让isSametree函数来比较这俩个
+//    // if(root!=NULL&&subRoot==NULL)
+//    //     return true;
+//
+//    if (root == NULL)
+//        return false;
+//
+//    if (_isSameTree(root, subRoot))
+//        return true;
+//
+//    //只要左右有一个是返回真，那就是子树
+//    return isSubtree(root->left, subRoot)
+//        || isSubtree(root->right, subRoot);
+//}
+
+
+//翻转二叉树
+//https://leetcode-cn.com/problems/invert-binary-tree/
+//void _invertTree(struct TreeNode* root)
+//{
+//    if (root == NULL)//设置退出条件，如果根节点为空就返回
+//        return;
+//
+//    //让另外两个值来接收原本的左右节点
+//    struct TreeNode* left = root->left;
+//    struct TreeNode* right = root->right;
+//
+//    //更改左右节点
+//    root->right = left;
+//    root->left = right;
+//
+//    //递归子树
+//    _invertTree(root->left);
+//    _invertTree(root->right);
+//}
+//
+//struct TreeNode* invertTree(struct TreeNode* root) {
+//    if (root == NULL)//判断空树
+//        return NULL;
+//
+//    _invertTree(root);
+//
+//    return root;
+//}
+
+
+//KY11 二叉树遍历
+//https://www.nowcoder.com/practice/4b91205483694f449f94c179883c1fef?tpId=60&&tqId=29483&rp=1&ru=/activity/oj&qru=/ta/tsing-kaoyan/question-ranking
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<stdbool.h>
+//
+//
+//typedef char BTDataType;
+//
+//typedef struct BTreeNode
+//{
+//    BTDataType data;
+//    struct BTreeNode* left;
+//    struct BTreeNode* right;
+//}BTNode;
+//
+//// 二叉树中序遍历 
+//void BTreeInOrder(BTNode* root)
+//{
+//    if (root == NULL) {
+//        return;
+//    }
+//
+//    BTreeInOrder(root->left);
+//    printf("%c ", root->data);
+//    BTreeInOrder(root->right);
+//}
+//
+//BTNode* CreatTree(char* arr, int* i)
+//{
+//    if (arr[*i] == '#') {
+//        (*i)++;
+//        return NULL;
+//    }
+//
+//    BTNode* newnode = (BTNode*)malloc(sizeof(BTNode));
+//    if (newnode == NULL)
+//    {
+//        printf("malloc failed\n");
+//        exit(-1);
+//    }
+//
+//    newnode->data = arr[(*i)++];
+//    newnode->left = CreatTree(arr, i);
+//    newnode->right = CreatTree(arr, i);
+//
+//    return newnode;
+//}
+//
+//int main()
+//{
+//    char arr[100];
+//    scanf("%s", arr);
+//
+//    int i = 0;
+//    BTNode* root = CreatTree(arr, &i);
+//    BTreeInOrder(root);
+//    return 0;
+//}
+
+
+//102. 二叉树的层序遍历
+//https://leetcode-cn.com/problems/binary-tree-level-order-traversal/submissions/
+//这道题写了题解，看自己的博客！
+
+//#define MAX 2000
+//int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes) {
+//    if (root == NULL)
+//        return;
+//
+//    struct TreeNode* Queue[MAX];//队列，存放节点的地址
+//    int front = 0, tail = 0;//指向队头和队尾
+//
+//    *returnSize = 0;//将二叉树层级初始化为0
+//    int** ret = (int**)malloc(sizeof(int*) * MAX);//存放二叉树的每一层节点的值
+//    *returnColumnSizes = (int*)malloc(sizeof(int*) * (MAX / 2));//开辟一个数组来存放每一层的节点个数
+//
+//    struct TreeNode* head;
+//    Queue[tail++] = root;//根节点入队
+//    while (front != tail)
+//    {
+//        int Csize = 0;//每一层的节点个数
+//        int end = tail;//end是每一层最末一个节点的指针。在后续的入队列操作中tail会改变，所以需要保存tail的值
+//        ret[*returnSize] = (int*)malloc(sizeof(int*) * (end - front));//为每一层开辟一个单独的数组来存放值
+//        while (front < end)
+//        {
+//            head = Queue[front++];
+//            ret[*returnSize][Csize++] = head->val;
+//            if (head->left != NULL)
+//                Queue[tail++] = head->left;
+//            if (head->right != NULL)
+//                Queue[tail++] = head->right;
+//        }
+//        (*returnColumnSizes)[*returnSize] = Csize;//得到每一层的节点数量
+//        (*returnSize)++;//层数+1
+//    }
+//
+//    return ret;
+//}
---
+++ LetsOJ/ewait/Leetcode/数据结构_链表1.c
@@ -0,0 +1,321 @@
+﻿//T1
+//4.输入一个链表，输出该链表中倒数第k个结点。 
+//OJ链接
+//https://www.nowcoder.com/practice/529d3ae5a407492994ad2a246518148a?tpId=13&&tqId=11167&rp=2&ru=/activity/oj&qru=/ta/coding-interviews/question-ranking
+
+
+/*
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x) :
+            val(x), next(NULL) {
+    }
+};*/
+//class Solution {
+//public:
+//    ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
+//        if (pListHead == NULL)
+//            return NULL;
+//        struct ListNode* fast = pListHead;
+//        struct ListNode* slow = pListHead;
+//
+//        int i = k;
+//        while (i--)
+//        {
+//            if (fast == NULL)
+//            {
+//                return NULL;
+//            }
+//            fast = fast->next;
+//        }
+//
+//
+//        while (fast)
+//        {
+//            fast = fast->next;
+//            slow = slow->next;
+//        }
+//
+//        return slow;
+//    }
+//};
+
+
+//t2
+// //将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的
+//https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+//struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+//    if (list1 == NULL)
+//        return list2;
+//    if (list2 == NULL)
+//        return list1;
+//
+//
+//    struct ListNode* H1 = list1;
+//    struct ListNode* H2 = list2;
+//    struct ListNode* newList = NULL;
+//    struct ListNode* tail = NULL;
+//    if (H1->val < H2->val)
+//    {
+//        newList = H1;
+//        tail = H1;
+//        H1 = H1->next;
+//    }
+//    else
+//    {
+//        newList = H2;
+//        tail = H2;
+//        H2 = H2->next;
+//    }
+//
+//
+//
+//    while (H1 && H2)
+//    {
+//        if (H1->val < H2->val)
+//        {
+//
+//
+//            tail->next = H1;
+//            tail = H1;//等同于tail=tail->next
+//            H1 = H1->next;
+//        }
+//        else
+//        {
+//            tail->next = H2;
+//            tail = H2;//等同于tail=tail->next
+//
+//
+//            H2 = H2->next;
+//        }
+//    }
+//
+//
+//    if (H1)
+//    {
+//        tail->next = H1;
+//    }
+//
+//
+//    if (H2)
+//    {
+//        tail->next = H2;
+//    }
+//
+//    return newList;
+//}
+
+
+//T3
+//编写代码，以给定值x为基准将链表分割成两部分，所有小于x的结点排在大于或等于x的结点之前 
+//https://www.nowcoder.com/practice/0e27e0b064de4eacac178676ef9c9d70?tpId=8&&tqId=11004&rp=2&ru=/activity/oj&qru=/ta/cracking-the-coding-interview/question-ranking
+
+///*
+//struct ListNode {
+//    int val;
+//    struct ListNode *next;
+//    ListNode(int x) : val(x), next(NULL) {}
+//};*/
+//class Partition {
+//public:
+//    ListNode* partition(ListNode* pHead, int x) {
+//        if (pHead == NULL)
+//            return NULL;
+//
+//        struct ListNode* newhead = (struct ListNode*)malloc(sizeof(struct ListNode));
+//        struct ListNode* Bigger = (struct ListNode*)malloc(sizeof(struct ListNode));
+//        struct ListNode* head = newhead;
+//        struct ListNode* big = Bigger;
+//
+//        while (pHead)
+//        {
+//            if (pHead->val < x)
+//            {
+//                head->next = pHead;
+//                pHead = pHead->next;
+//                head = head->next;
+//
+//            }
+//            else
+//            {
+//                big->next = pHead;
+//                pHead = pHead->next;
+//                big = big->next;
+//            }
+//        }
+//        big->next = NULL;
+//        head->next = Bigger->next;
+//
+//        struct ListNode* list = newhead->next;
+//        free(newhead);
+//        free(Bigger);
+//        return list;
+//    }
+//};
+
+
+//t4
+//7.链表的回文结构。
+//https://www.nowcoder.com/practice/d281619e4b3e4a60a2cc66ea32855bfa?tpId=49&&tqId=29370&rp=1&ru=/activity/oj&qru=/ta/2016test/question-ranking
+
+///*
+//struct ListNode {
+//    int val;
+//    struct ListNode *next;
+//    ListNode(int x) : val(x), next(NULL) {}
+//};*/
+//class PalindromeList {
+//public:
+//    //链表逆置（3.14）
+//    struct ListNode* reverseList(struct ListNode* head) {
+//
+//
+//        struct ListNode* newhead = NULL;
+//        struct ListNode* cur = head;
+//        while (cur)
+//        {
+//            struct ListNode* next = cur->next;
+//            cur->next = newhead;
+//
+//
+//            newhead = cur;
+//            cur = next;
+//        }
+//
+//
+//
+//
+//        return newhead;
+//    }
+//
+//    //中间节点返回（3.14的oj题目）
+//    //1 2 3 4  偶数个，返回3
+//    //1 2 3    奇数个，返回2
+//    struct ListNode* middleNode(struct ListNode* head) {
+//        if (head == NULL)
+//            return NULL;
+//
+//
+//        int count = 1;//计算链表一共的节点数
+//        struct ListNode* tail = head;//找尾
+//        while (tail->next != NULL)
+//        {
+//            tail = tail->next;
+//            count++;
+//        }
+//        int half = (count / 2);
+//        //如果是3结果为1，如果是4结果为2
+//        //正好是需要从head开始寻找的次数
+//        struct ListNode* mid = head;
+//        while ((mid->next != NULL) && (half--))
+//        {
+//            mid = mid->next;
+//        }
+//
+//
+//        return mid;
+//    }
+//
+//    bool chkPalindrome(ListNode* A) {
+//        struct ListNode* mid = middleNode(A);
+//        struct ListNode* ret = reverseList(mid);
+//        //1 2 3 2 1 回文链表
+//        //先逆转后面3个
+//        //1 2 1 2 3
+//        //这时候第二个2存放的next依旧是下一个3的地址
+//        //使用while循环进行判断，都是判断3次是否相等
+//        //相等就是回文，否则不是
+//        while (A && ret)
+//        {
+//            if (A->val == ret->val)
+//            {
+//                A = A->next;
+//                ret = ret->next;
+//            }
+//            else
+//            {
+//                return false;
+//            }
+//        }
+//        return true;
+//
+//
+//    }
+//};
+
+
+//判断链表相交（3.19的第一题）
+//https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* getIntersectionNode(struct ListNode* headA, struct ListNode* headB) {
+    if ((headA == NULL) || (headB == NULL))//服了，找了几十分钟的错误，发现是等于号写错了，==写成了=
+        return NULL;
+
+    int countA = 1;
+    int countB = 1;
+    struct ListNode* HA = headA;
+    struct ListNode* HB = headB;
+    while (HA && HA->next)
+    {
+        countA++;
+        HA = HA->next;
+    }
+    while (HB && HB->next)
+    {
+        countB++;
+        HB = HB->next;
+    }
+
+    if (HB != HA)//结尾不相等，说明不相交
+        return NULL;
+
+    int num = abs(countA - countB);
+    struct ListNode* fast = NULL;
+    struct ListNode* slow = NULL;
+    if (countA > countB)
+    {
+        fast = headA;
+        slow = headB;
+    }
+    else
+    {
+        fast = headB;
+        slow = headA;
+    }
+
+    while (num--)
+    {
+        fast = fast->next;
+    }
+
+    while (fast && slow)
+    {
+        if (fast == slow)
+            return fast;
+
+        fast = fast->next;
+        slow = slow->next;
+    }
+
+    return NULL;
+}
+
