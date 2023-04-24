90be93d062986d0dc0ee63dd185f2d3591be2a7d
musnow <ezplayingd@126.com>
2023-04-24 12:39:04 +0800

    24.04.24 151leetcode更新 方案3

--- LetsOJ/musnow/Leetcode/151反转字符串中的单词.cpp
+++ LetsOJ/musnow/Leetcode/151反转字符串中的单词.cpp
@@ -1,3 +1,4 @@
+////https://leetcode.cn/problems/reverse-words-in-a-string/
 class Solution {
 public:
     //从begin开始找到下一个空格的下标，不包括begin本身
@@ -49,7 +50,7 @@ public:
         return ret;
     }
 };
-//https://leetcode.cn/problems/reverse-words-in-a-string/
+
 //上面的办法中，新增了一个vector来拼接字符串，空间复杂度O(N)
 //24MS 7.1MB
 
@@ -159,3 +160,118 @@ public:
     }
 };
 //4MS 7MB
+
+//上面的办法看起来很对，实际上还是不是原地算法
+//因为我每次都是在后面尾随字符串，而没有把原字符串删掉。如果源字符串很长，我这么做就相当于把源字符串的长度变为原有俩倍
+//依旧是O(N)的空间复杂度
+//要想做到真原地，就需要在插入到指定位置的同时，将其原本的字符串给删除了
+
+//方案3
+class Solution {
+public:
+    #define SEP '|'
+    //从begin开始找到下一个空格的下标，不包括begin本身
+    int find_space(string&s,int begin=0)
+    {
+        int i=0;
+        for(i=begin+1;i<s.size();i++)
+        {
+            if(s[i]==' '){
+                break;
+            }
+            else if(s[i]==SEP)
+            {//用一个特殊字符作为原字符串结尾
+                break;
+            }
+        }
+        //如果是因为找到空格跳出，判断该字符串后面还有没有有效字符
+        int k = i;//不能改i
+        if(s[k]!=SEP)
+        {
+            while(s[k]==' ')
+            {
+                k++;
+                //一直走到了|，代表没有有效字符了
+                if(s[k]==SEP){
+                    return s.npos;
+                }
+            }
+            //走到这里代表有
+            return i;
+        }
+
+        return s.npos;
+    }
+
+    string reverseWords(string s) {
+        int i=0,end_word=0,index=0;
+        int begin_sz = s.size();//起始大小
+        s+=SEP;//用一个特殊字符作为原字符串结尾
+        int sep_index = s.size()-1;//最后一个字符的位置就是分隔符
+        
+        for(i=0;i<begin_sz;i++)
+        {
+            // 跳过前导空格
+            while(s[i]==' '){
+                i++;
+            }
+            // 如果已经到最后面了，就直接跳出
+            if(i>=begin_sz){
+                break;
+            }
+            index = find_space(s,i);
+            //如果找不到下一个空格了，代表当前是原字符串最后一个单词
+            //跳过
+            if(index == s.npos){
+                end_word=i;
+                break;
+            }
+            int sz = index-i;//单词长度
+            string temp(s,i,sz);
+            temp+=" ";
+            //不能尾插，应该在sep之后插入
+            //s += temp;
+            s.insert(sep_index+1,temp);
+            //printf("[%d-%d-1]%s\n",i,sep_index,s.c_str());
+
+            // cout << i << " | " << index << " | "<< sz << endl;
+            // cout << temp << "-" << endl;
+
+            //构造了之后，把原str这部分删了(错误！删了之后下标变了，循环无意义)
+            s.erase(i,sz);
+            sep_index-=sz;
+            //printf("[%d-%d-2]%s\n",i,sep_index,s.c_str());
+
+            // i = i;//跳过当前单词
+            //index-1是因为本次循环过了之后会被for给+1
+        }
+        //cout <<"[end]"<< s << endl;
+
+        index = s.find(SEP);
+        // 删除index之前,end_word之后的空格
+        int k = s.find(" ",0);
+        //cout << "k:" <<k<<" endw:" << end_word <<endl;
+        while(k!=s.npos&&k<index){
+            s.replace(k,1,"");
+            index--;
+            //cout <<"[while1]k:"<<k <<" str:"<< s << endl;
+            //因为替换过了之后，k的位置变成了原有k的下一位
+            //此时如果k还是空格，代表原字符串末尾有多个空格
+            //继续删除，但是每次删除都需要让sep的位置-1
+            while(s[k]==' '){
+                s.replace(k,1,"");
+                index--;
+            }
+            //跳出这个循环的时候，代表碰到sep分隔符了,字符串已经处理完毕
+            //cout <<"[while2]k:"<<k <<" str:"<< s << endl;
+            k = s.find(" ",0);
+        }
+        //cout <<"[end2]"<< s << endl;
+        s[index]=' ';
+        //s.erase(0,end_word);//删掉前面的原有单词
+        s.erase(s.size()-1,1);//删除最后单词带的空格
+        //cout <<"[return]"<< s << endl;
+        return s;
+    }
+};
+
