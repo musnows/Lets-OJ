91559046afabe890f4a9c477fd483b75c078514d
poolblue7 <1264163816@qq.com>
2023-10-19 13:26:54 +0800

    2023.10.19 适龄的朋友

---
+++ oj/poolblue/leetcode/适龄的朋友.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.cn/problems/friends-of-appropriate-ages/solutions/1174365/gua-ling-de-peng-you-by-leetcode-solutio-v7yk/
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+        vector<int> cnt(121);
+        for (int age: ages) {
+            ++cnt[age];
+        }
+        vector<int> pre(121);
+        for (int i = 1; i <= 120; ++i) {
+            pre[i] = pre[i - 1] + cnt[i];
+        }
+        int ans = 0;
+        for (int i = 15; i <= 120; ++i) {
+            if (cnt[i]) {
+                int bound = i * 0.5 + 8;
+                ans += cnt[i] * (pre[i] - pre[bound - 1] - 1);
+            }
+        }
+        return ans;
+    }
+};
+
