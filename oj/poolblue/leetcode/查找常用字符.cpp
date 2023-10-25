8815d06c9d877d60f0058e94c34d3e310b93eb0d
poolblue7 <1264163816@qq.com>
2023-10-25 23:49:16 +0800

    2023.10.25 查找常用字符

---
+++ oj/poolblue/leetcode/查找常用字符.cpp
@@ -0,0 +1,52 @@
+//https://leetcode.cn/problems/find-common-characters/solutions/445468/cha-zhao-chang-yong-zi-fu-by-leetcode-solution/https://leetcode.cn/problems/find-common-characters/solutions/445468/cha-zhao-chang-yong-zi-fu-by-leetcode-solution/
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<int> minfreq(26, INT_MAX);
+        vector<int> freq(26);
+        for (const string& word: words) {
+            fill(freq.begin(), freq.end(), 0);
+            for (char ch: word) {
+                ++freq[ch - 'a'];
+            }
+            for (int i = 0; i < 26; ++i) {
+                minfreq[i] = min(minfreq[i], freq[i]);
+            }
+        }
+
+        vector<string> ans;
+        for (int i = 0; i < 26; ++i) {
+            for (int j = 0; j < minfreq[i]; ++j) {
+                ans.emplace_back(1, i + 'a');
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<int> minfreq(26, INT_MAX);
+        vector<int> freq(26);
+        for (const string& word: words) {
+            fill(freq.begin(), freq.end(), 0);
+            for (char ch: word) {
+                ++freq[ch - 'a'];
+            }
+            for (int i = 0; i < 26; ++i) {
+                minfreq[i] = min(minfreq[i], freq[i]);
+            }
+        }
+
+        vector<string> ans;
+        for (int i = 0; i < 26; ++i) {
+            for (int j = 0; j < minfreq[i]; ++j) {
+                ans.emplace_back(1, i + 'a');
+            }
+        }
+        return ans;
+    }
+};
+
+ 
